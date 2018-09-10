219e7a88520a4597ba2ba7d4074cee685d983c94
FelixStarke <starke.felix@t-online.de>
2018-09-10 19:10:09 +0200

    Fixed indentation.

--- youtube-embedded-viewer/background.js
+++ youtube-embedded-viewer/background.js
@@ -1,17 +1,17 @@
-chrome.runtime.onInstalled.addListener(function() {
+chrome.runtime.onInstalled.addListener(function () {
 
     // 0: this tab
     // 1: new tab
     // 2: new window
     // 3: popup
-    chrome.storage.sync.set({openIn: 0, closeTab: false}, null);
-    
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-	chrome.declarativeContent.onPageChanged.addRules([{
-	    conditions: [
-		new chrome.declarativeContent.PageStateMatcher({pageUrl: {urlContains: 'youtube.com/watch?'}})
-	    ],
-	    actions: [new chrome.declarativeContent.ShowPageAction()]
-	}]);
+    chrome.storage.sync.set({ openIn: 0, closeTab: false }, null);
+
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [
+                new chrome.declarativeContent.PageStateMatcher({ pageUrl: { urlContains: 'youtube.com/watch?' } })
+            ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
     });
 });
